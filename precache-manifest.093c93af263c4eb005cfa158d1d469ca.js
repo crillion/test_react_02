de2b0e1bda9e77eedfdf975fec30a69dd52ad1e6
Marco Pappalepore <crillion@tiscali.it>
2020-02-16 12:10:35 +0100

    Updates

---
+++ precache-manifest.093c93af263c4eb005cfa158d1d469ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fce5c7de2fb404123edf92f5542165",
+    "url": "/test_react_02/index.html"
+  },
+  {
+    "revision": "a57f87902b9aff22305a",
+    "url": "/test_react_02/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "321518e1179642a99007",
+    "url": "/test_react_02/static/js/2.6cdd38eb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test_react_02/static/js/2.6cdd38eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57f87902b9aff22305a",
+    "url": "/test_react_02/static/js/main.8d488219.chunk.js"
+  },
+  {
+    "revision": "e7941f047653c90b196a",
+    "url": "/test_react_02/static/js/runtime-main.634194dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_react_02/static/media/logo.5d5d9eef.svg"
+  }
+]);
